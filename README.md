6731f339316b6f45ebb10d13868d311ffe5354a4
Mai1126 <mai.11260923@gmail.com>
2019-05-24 01:15:38 +0900

    課題3修了

--- README.md
+++ README.md
@@ -42,3 +42,27 @@ $ curl -XGET -H 'Content-Type:application/json' http://localhost:8080/
 ```
 {"message":"Hello World!!"}
 ```
+
+### user を作成
+リクエスト
+```
+curl -XPOST -H 'Content-Type:application/json' http://localhost:8080/users -d '{"name": "test", "email": "hoge@example.com" }'
+```
+結果
+```
+{"id":1,"name":"test","email":"hoge@example.com","created_at":"2019-05-23T23:45:16.245937Z","updated_at":"2019-05-23T23:45:16.245937Z"}
+```
+
+### user を一覧
+```
+curl -XGET -H 'Content-Type:application/json' http://localhost:8080/users
+```
+結果
+```
+[{"id":1,"name":"test","email":"hoge@example.com","created_at":"2019-05-23T23:42:25.660355Z","updated_at":"2019-05-23T23:42:25.660355Z"},{"id":2,"name":"test","email":"hoge@example.com","created_at":"2019-05-23T23:43:04.572914Z","updated_at":"2019-05-23T23:43:04.572914Z"},{"id":3,"name":"test","email":"hoge@example.com","created_at":"2019-05-23T23:45:16.245937Z","updated_at":"2019-05-23T23:45:16.245937Z"}]
+```
+
+### user を削除
+```
+$ curl -XDELETE -H 'Content-Type:application/json' http://localhost:8080/users/1
+```
--- kadai3/app_go/hello.go
+++ kadai3/app_go/hello.go
@@ -5,8 +5,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
+	_ "github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
@@ -19,13 +22,15 @@ type Input struct {
 	Email string `json:"email"`
 }
 
-type Output struct {
-	CreatedAt string `json:"created_at"`
-	Email     string `json:"email"`
+type User struct {
 	ID        int    `json:"id"`
 	Name      string `json:"name"`
+	Email     string `json:"email"`
+	CreatedAt string `json:"created_at"`
 	UpdatedAt string `json:"updated_at"`
 }
+
+type Users []User
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	index := Index{"Hello World!!"}
@@ -64,32 +69,126 @@ func usersHandler(w http.ResponseWriter, r *http.Request) {
 			fmt.Println(err.Error())
 			return
 		}
-		var id int
-		err = db.QueryRow("INSERT INTO users (name, email) VALUES($1,$2) RETURNING id", input.Name, input.Email).Scan(&id)
+		user := User{}
+		err = db.QueryRow("INSERT INTO users (name, email) VALUES($1,$2) RETURNING id, name, email, created_at, updated_at", input.Name, input.Email).Scan(&user.ID, &user.Name, &user.Email, &user.CreatedAt, &user.UpdatedAt)
 		if err != nil {
 			fmt.Println(err)
 		}
 
-		var row string
-		err = db.QueryRow("SELECT * FROM user where id = ?", id).Scan(&row)
-		res, err := json.Marshal(row)
+		res, err := json.Marshal(user)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(201)
 		w.Write(res)
-
 	}
 
-	// if r.Method == "GET" {
+	if r.Method == "GET" {
+		rows, err := db.Query("SELECT id, name, email, created_at, updated_at FROM users")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+		var users Users
+		user := User{}
 
-	// }
+		for rows.Next() {
+			if err := rows.Scan(&user.ID, &user.Name, &user.Email, &user.CreatedAt, &user.UpdatedAt); err != nil {
+				log.Fatal(err)
+			}
+			users = append(users, user)
+		}
+		res, err := json.Marshal(users)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		w.Write(res)
+	}
 
-	// if r.Method == "DELETE" {
-	// }
+}
+
+func usersidHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	db, err := sql.Open("postgres", "host=postgres port=5432 user=root password=password dbname=Godb sslmode=disable")
+	defer db.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if r.Method == "DELETE" {
+		_, err := db.Exec("delete from users WHERE id = '" + id + "'")
+		if err != nil {
+			fmt.Println(err)
+		}
+		w.WriteHeader(204)
+	}
+
+	if r.Method == "GET" {
+		user := User{}
+		err = db.QueryRow("SELECT id, name, email, created_at, updated_at FROM users WHERE id = '"+id+"' LIMIT 1").Scan(&user.ID, &user.Name, &user.Email, &user.CreatedAt, &user.UpdatedAt)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		res, err := json.Marshal(user)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		w.Write(res)
+	}
+
+	if r.Method == "PUT" {
+		if r.Header.Get("Content-Type") != "application/json" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		input := Input{}
+		err = json.Unmarshal(body, &input)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		_, err = db.Exec("UPDATE users SET name = '" + input.Name + "', email = '" + input.Email + "' WHERE id = " + id)
+		if err != nil {
+			fmt.Println(err)
+		}
+		user := User{}
+		err = db.QueryRow("SELECT id, name, email, created_at, updated_at FROM users WHERE id = '"+id+"' LIMIT 1").Scan(&user.ID, &user.Name, &user.Email, &user.CreatedAt, &user.UpdatedAt)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		res, err := json.Marshal(user)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		w.Write(res)
+	}
 
 }
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/users", usersHandler)
-	http.ListenAndServe(":8080", nil)
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", indexHandler)
+	myRouter.HandleFunc("/users", usersHandler)
+	myRouter.HandleFunc("/users/{id}", usersidHandler)
+	http.ListenAndServe(":8080", myRouter)
 }
